258950a95c04889f9a981355c8bc171f0026645b
Tomas Jakubec <jakubto1@fjfi.cvut.cz>
2020-06-10 14:02:16 +0200

    Simple unit test

---
+++ src/UnitTests/main.h
@@ -0,0 +1,15 @@
+#ifdef HAVE_GTEST
+#include <gtest/gtest.h>
+#else
+#include "GtestMissingError.h"
+#endif
+
+int main( int argc, char* argv[] )
+{
+#ifdef HAVE_GTEST
+   ::testing::InitGoogleTest( &argc, argv );
+   return RUN_ALL_TESTS();
+#else
+   throw GtestMissingError();
+#endif
+}
